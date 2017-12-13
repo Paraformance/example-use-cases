ca0dd6edc9b3fe75d12018e4404dfa731038a01b
Dibayendu Dey <dibayendu.dey@gmail.com>
2017-12-13 16:21:47 +0000

    addvec code with makefile

---
+++ eclipse/addvec/addvec.cpp
@@ -0,0 +1,50 @@
+/*
+ * addvec.cpp
+ *
+ *  Created on: 8 Dec 2015
+ *      Author: kwxm
+ */
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <tbb/tbb.h>
+
+using namespace std;
+using namespace tbb;
+
+void addvec (const vector<int> &a,  const vector<int> &b, vector<int> &c, int &k) {
+	if (a.size() != b.size() || a.size() != c.size()) {
+		cerr << "error: vector size mismatch\n";
+		exit(0);
+	}
+
+	for (unsigned int i = 0; i != a.size(); ++i) {
+		c[i] = a[i] + b[i];
+		k = k + 1;
+		cout << c[i] << endl;
+	}
+}
+
+const int N=5000;
+
+int main () {
+	int k=0;
+
+	auto x = vector<int>(N);
+	auto y = vector<int>(N);
+	auto z = vector<int>(N);
+
+	for (int j=0; j<N; j++) {
+			x[j]=j;
+			y[j]=j+1;
+	}
+
+	addvec(x,y,z,k);
+
+	for (unsigned int i=0; i<z.size(); i++) {
+		cout << i << " -> " << z[i] << endl;
+	}
+
+	cout << endl << "k=" << k << endl;
+}
+
